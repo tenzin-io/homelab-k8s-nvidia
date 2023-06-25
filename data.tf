f11a05d2f0e89eaaf3ab105df0b4495c252ae7f6
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-06-25 11:26:53 -0400

    Rename secret store root path from secret to github

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "vault_generic_secret" "github_app" {
-  path = "secret/github_app"
+  path = "github/github_app"
 }
 
 data "vault_generic_secret" "cloudflare" {
-  path = "secret/cloudflare"
+  path = "github/cloudflare"
 }
