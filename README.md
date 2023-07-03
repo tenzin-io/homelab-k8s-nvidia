52e44a5cdd01e969fb5af2ec9b97a9fa3d9a165d
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-07-03 14:34:58 -0400

    Update github_actions to use release versions

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ A Terraform configuration repository to manage my home lab Kubernetes cluster wi
 | Name | Source | Version |
 |------|--------|---------|
 | <a name="module_cert_manager"></a> [cert\_manager](#module\_cert\_manager) | git::https://github.com/tenzin-io/terraform-tenzin-cert-manager.git | v0.0.2 |
-| <a name="module_github_actions"></a> [github\_actions](#module\_github\_actions) | git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git | v0.1.0 |
+| <a name="module_github_actions"></a> [github\_actions](#module\_github\_actions) | git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git | v0.2.0 |
 | <a name="module_nvidia_device_plugin"></a> [nvidia\_device\_plugin](#module\_nvidia\_device\_plugin) | git::https://github.com/tenzin-io/terraform-tenzin-nvidia-device-plugin.git | v0.1.0 |
 
 ## Resources
--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ module "cert_manager" {
 }
 
 module "github_actions" {
-  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git?ref=v0.1.0"
+  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git?ref=v0.2.0"
   github_org_name            = "tenzin-io"
   github_app_id              = data.vault_generic_secret.github_app.data.app_id
   github_app_installation_id = data.vault_generic_secret.github_app.data.installation_id
   github_app_private_key     = data.vault_generic_secret.github_app.data.private_key
   github_runner_labels       = ["homelab", "nvidia"]
-  github_runner_image        = "containers.tenzin.io/docker/tenzin-io/actions-runner-images/nvidia-latest:v0.0.1"
+  github_runner_image        = "containers.tenzin.io/docker/tenzin-io/actions-runner-images/nvidia-latest:v0.0.4"
   enable_nvidia_gpu          = true
 }
 
